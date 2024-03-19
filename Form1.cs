1f5f48a01b283501b51859c97e096edd4e6979ec
Zyad Abdul-Nasser <zizo_007@live.com>
2024-03-19 13:55:11 +0200

    zyad

--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +13,41 @@ namespace GUI_Serial_Interface
 {
     public partial class Form1 : Form
     {
+        private int pointX = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            chart1.Series["Series1"].Points.AddXY(pointX, 20);
+            pointX++;
+        }
+
+        private void AddPointToChart(int x, int y) 
+        {
+            chart1.Series["Series1"].Points.AddXY(x, y);
+        }
+
+        public void ChartChildThread()
+        {
+            new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+
+                while (true)
+                {
+                    chart1.Series["Series1"].Points.AddXY(pointX, 20);
+                    pointX++;
+                }
+            }).Start();
+
         }
     }
 }
